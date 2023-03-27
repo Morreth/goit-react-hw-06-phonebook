932602e21617b710e59f2ae7108a953f6cb8d2fa
Olha Brylova <47885094+Morreth@users.noreply.github.com>
2023-03-27 21:23:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,24 @@
-
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import ContactForm from './Contacts/ContactsForm';
 import ContactList from './Contacts/ContactList';
 import  Filter  from './Contacts/FilterContacts';
 
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-    const changeFilter = e => {
+  const changeFilter = e => {
       setFilter(e.currentTarget.value );
-    };
-
-    const handleSubmit = obj => {
+   };
+  
+  const handleSubmit = obj => {
      
       const checkContact = contacts.find(
         contact => contact.name.toLowerCase() === obj.name.toLowerCase()
       );
       if (!checkContact) {
         setContacts( contacts => [...contacts, obj] );
-        // console.log(obj)
         return;
       }
       alert(`${obj.name} is already in contacts `);
@@ -50,7 +35,7 @@ const App = () => {
         contact.name.toLowerCase().includes(normalize)
       );
     };
-    
+   
 
       return (
         <div>
@@ -67,5 +52,3 @@ const App = () => {
     
 };
 export default App;
-
-
